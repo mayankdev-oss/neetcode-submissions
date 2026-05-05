ec2b882658a96d6f5a2568f643dba38e02010877
Mayank <MAYANKJOSHI.25101280629@gehu.ac.in>
2026-05-05 21:41:47 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i=0, j=0;
+        for(i=0; i<nums.size()-1; i++){
+            for(j=i+1; j<nums.size(); j++){
+                if(nums[j]+nums[i]==target){
+                    return {i, j};
+                }
+            }
+        }
+    }
+};
