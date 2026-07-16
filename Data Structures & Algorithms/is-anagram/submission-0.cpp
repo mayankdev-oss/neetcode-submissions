786da71ec6c18a5de19357d1c37a87ac7219dc63
Mayank <MAYANKJOSHI.25101280629@gehu.ac.in>
2026-07-16 21:43:34 +0530

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.cpp
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-            if(s==t){
-                return true;
-            }
-        
-        return false;
+        unordered_map<char, int> mp1;
+        unordered_map<char, int> mp2;
+        if(s.size()!=s.size()){
+            return false;
+        }
+        for(char x: s){
+            mp1[x]++;
+        }
+        for(char x: t){
+            mp2[x]++;
+        }
+
+        if(mp1==mp2){
+            return true;
+        }else{
+            return false;
+        }
+
+
     }
 };
